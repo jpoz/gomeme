071d5c6fea6f172487a9d146e3719aa54d77ee4e
James Pozdena <jpoz@jpoz.net>
2016-12-03 18:17:39 -0800

    Switch statement for content types

--- cmd/gomeme/gomeme.go
+++ cmd/gomeme/gomeme.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/jpoz/gomeme"
@@ -47,19 +52,39 @@ func main() {
 		fail("Need input and output path\n\n", nil)
 	}
 
-	in, err := os.Open(flag.Arg(0))
+	in, err := ioutil.ReadFile(flag.Arg(0))
 	if err != nil {
 		fail("Could not open input", err)
 	}
 
-	g, err := gif.DecodeAll(in)
-	if err != nil {
-		fail("Failed to decode gif", err)
+	meme := &gomeme.Meme{
+		Config: config,
 	}
 
-	meme := &gomeme.Meme{
-		Config:   config,
-		Memeable: gomeme.GIF{g},
+	contentType := http.DetectContentType(in)
+	buff := bytes.NewBuffer(in)
+
+	switch contentType {
+	case "image/gif":
+		g, err := gif.DecodeAll(buff)
+		if err != nil {
+			fail("Failed to decode gif", err)
+		}
+		meme.Memeable = gomeme.GIF{g}
+	case "image/jpeg":
+		j, err := jpeg.Decode(buff)
+		if err != nil {
+			fail("Failed to decode jpeg", err)
+		}
+		meme.Memeable = gomeme.JPEG{j}
+	case "image/png":
+		p, err := png.Decode(buff)
+		if err != nil {
+			fail("Failed to decode png", err)
+		}
+		meme.Memeable = gomeme.PNG{p}
+	default:
+		fail(fmt.Sprintf("No idea what todo with a %s", contentType), nil)
 	}
 
 	out, err := os.Create(flag.Arg(1))
